49441b311b37dc0425274db40305185f3c92d5f3
Christian Eckenrode Work <christianeckenrodework@Christians-MacBook-Pro.local>
2016-10-03 15:22:34 -0400

    Added reducers from libraries and selected libraries, added action for selecting library

---
+++ src/components/LibraryList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { ListView } from 'react-native';
+import ListItem from './ListItem';
+
+class LibraryList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+  render() {
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
+  }
+  renderRow(library) {
+    return (
+      <ListItem
+        library={library}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
